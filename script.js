50f626f6ae37d461834aef953df6850d389c1bec
Claudia S Cabrera <miradacsc@gmail.com>
2023-05-27 07:23:04 -0300

    correcion boton copiar y css

--- script.js
+++ script.js
@@ -1,40 +1,125 @@
-function performOperation() {
-    var inputText = document.getElementById('input').value;
-    var encryptRadio = document.getElementById('encryptRadio');
-    var outputText = '';
+const textArea = document.querySelector("#input");
+const mensaje = document.querySelector("#output");
+const copia = document.querySelector("#copySuccess");
+
+function validarTexto() {
+  let textoEscrito = textArea.value;
+  let validador = textoEscrito.match(/^[a-z]*$/);
+
+  if (!validador || validador.length === 0) {
+    swal({
+      icon: "error",
+      title: "Oops...",
+      text: "Solo se permiten letras minúsculas y sin acentos",
+    }).then(() => {
+      location.reload();
+    });
+    return true;
+  }
   
-    if (encryptRadio.checked) {
-      outputText = encrypt(inputText);
-    } else {
-      outputText = decrypt(inputText);
+  /*if (!validador || validador.length === 0) {
+    alert("Solo se permiten letras minúsculas y sin acentos");
+    location.reload();
+    return true;
+  }*/
+}
+
+function btnEncriptar() {
+  if (!validarTexto()) {
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    mensaje.style.backgroundImage = "none";
+    textArea.value = "";
+    copia.style.display = "none";// Ocultar el mensaje de copia
+  }
+}
+
+// Llaves de encriptación
+// `La letra "e" es convertida para "enter"`
+// `La letra "i" es convertida para "imes"`
+// `La letra "a" es convertida para "ai"`
+// `La letra "o" es convertida para "ober"`
+// `La letra "u" es convertida para "ufat"`
+
+function encriptar(stringEncriptada) {
+  let matrizCodigo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  stringEncriptada = stringEncriptada.toLowerCase();
+
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringEncriptada.includes(matrizCodigo[i][0])) {
+      stringEncriptada = stringEncriptada.replaceAll(
+        matrizCodigo[i][0],
+        matrizCodigo[i][1]
+      );
     }
-  
-    document.getElementById('output').value = outputText;
   }
-  
-  function encrypt(text) {
-    var encryptedText = text.replace(/e/g, 'enter');
-    encryptedText = encryptedText.replace(/i/g, 'imes');
-    encryptedText = encryptedText.replace(/a/g, 'ai');
-    encryptedText = encryptedText.replace(/o/g, 'ober');
-    encryptedText = encryptedText.replace(/u/g, 'ufat');
-    return encryptedText;
+  return stringEncriptada;
+}
+
+function btnDesencriptar() {
+  const textoDesencriptado = desencriptar(textArea.value);
+  mensaje.value = textoDesencriptado;
+  textArea.value = "";
+}
+
+function desencriptar(stringDesencriptada) {
+  let matrizCodigo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  stringDesencriptada = stringDesencriptada.toLowerCase();
+
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+      stringDesencriptada = stringDesencriptada.replaceAll(
+        matrizCodigo[i][1],
+        matrizCodigo[i][0]
+      );
+    }
   }
-  
-  function decrypt(text) {
-    var decryptedText = text.replace(/enter/g, 'e');
-    decryptedText = decryptedText.replace(/imes/g, 'i');
-    decryptedText = decryptedText.replace(/ai/g, 'a');
-    decryptedText = decryptedText.replace(/ober/g, 'o');
-    decryptedText = decryptedText.replace(/ufat/g, 'u');
-    return decryptedText;
-  }
-  
-  function resetOutput() {
-    document.getElementById('output').value = '';
-  }
-  
-  function copyText() {
-    var outputText = document.getElementById('output');
-    outputText.select
-  }
+  return stringDesencriptada;
+}
+
+function copiar() {
+  mensaje.select();
+  navigator.clipboard.writeText(mensaje.value);
+  swal({
+    icon: "success",
+    title: "¡Texto copiado!",
+  });
+  /*
+  mensaje.value = "";
+  alert("Texto Copiado");*/
+  copia.style.display = "block"; // Mostrar el mensaje de copia
+}
+
+function resetOutput() {
+  mensaje.value = "";
+  //document.getElementById("output").value = "";
+}
+
+function copyText() {
+  var outputText = document.getElementById("output");
+  outputText.select();
+  document.execCommand("copy");
+  swal({
+    icon: "success",
+    title: "¡Texto copiado!",
+  });
+  copia.style.display = "block"
+  /*
+  alert("Texto copiado");
+  copia.style.display = "block";*/
+  //var outputText = document.getElementById("output");
+  //outputText.select();
+}
+
